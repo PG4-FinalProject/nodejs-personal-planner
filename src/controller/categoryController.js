b2ee8fd346d5bf09e43922a9858544f19c20f23f
yschoi <yschoi9733@gmail.com>
2025-01-22 19:13:30 +0900

    feat: 카테고리 목록 조회 구현

--- src/controller/categoryController.js
+++ src/controller/categoryController.js
@@ -1,7 +1,17 @@
+const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 
 const getAllCategories = (req, res) => {
-  return res.status(StatusCodes.OK).end();
+  let sql = `SELECT * FROM category`;
+  conn.query(sql, (err, result) => {
+    if (err) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: '카테고리 조회 실패!',
+      });
+    }
+
+    return res.status(StatusCodes.OK).json(result);
+  });
 };
 
 module.exports = {
