bdbcd584a57a7ea0f74aa9477c615e8c2003d77d
yschoi <yschoi9733@gmail.com>
2025-01-28 22:58:52 +0900

    feat: getStatistics 모듈 구현

--- src/routes/statistics.js
+++ src/routes/statistics.js
@@ -1,8 +1,9 @@
+const ensureAuthorization = require('../middlewares/auth.js');
 const express = require('express');
 const router = express.Router();
 
 const { getStatistics } = require('../controller/statisticController.js');
 
-router.get('/', getStatistics);
+router.get('/', ensureAuthorization, getStatistics);
 
 module.exports = router;
